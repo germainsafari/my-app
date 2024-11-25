80c5195a302db3c271542f9de552f58b87291449
Safari Germain <sfrgermain1@gmail.com>
2024-11-25 16:26:22 +0000

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/testnodejs-b5gac5eyfwdrcmgf.germanywestcentral-01.azurewebsites.net",
-  reactStrictMode: true,
-  // ... other Next.js configurations
+  /* config options here */
 };
-
 
 export default nextConfig;
